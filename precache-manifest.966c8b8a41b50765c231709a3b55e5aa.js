4ef973cfb9fa096721a74901d16928bd393ef67e
Mykhalenych <mykhalenych@gmail.com>
2020-07-07 16:11:44 +0300

    Updates

---
+++ precache-manifest.966c8b8a41b50765c231709a3b55e5aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71016e9c910ee40a6c6cd4872fa4734c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba50cab67f76e2fd872c",
+    "url": "/static/css/main.7c06e7e7.chunk.css"
+  },
+  {
+    "revision": "054668fc2a8ed9134079",
+    "url": "/static/js/2.a1f5ccbb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a1f5ccbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba50cab67f76e2fd872c",
+    "url": "/static/js/main.5e9e08ee.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
